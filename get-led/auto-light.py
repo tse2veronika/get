0d88d562a156c7da1485930c4ae74dc02fdb5061
tse2veronika <tsetva.vi@phystech.edu>
2026-02-11 14:24:13 +0300

    добавлен код прыгающего огонька

--- get-led/auto-light.py
+++ get-led/auto-light.py
@@ -8,9 +8,10 @@ GPIO.setup(p, GPIO.IN)
 state = 0
 while True:
     if GPIO.input(p):
+        state = 0
         GPIO.output(led, state)
-        time.sleep(0.3)
+        time.sleep(0.2)
     else:
-        state = not state
+        state = 1
         GPIO.output(led, state)
-        time.sleep(0.3)
+        time.sleep(0.2)
