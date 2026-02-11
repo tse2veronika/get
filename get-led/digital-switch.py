a45c4b7a2fb599b0de81fb87bd7bb99c1c034650
tse2veronika <tsetva.vi@phystech.edu>
2026-02-11 13:06:40 +0300

    добавлен код вкл/выкл

---
+++ get-led/digital-switch.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+button = 13
+led = 26
+GPIO.setup(button, GPIO.IN)
+state = 0
+while True:
+    if GPIO.input(button):
+        state = not state
+        GPIO.output(led, state)
+        time.sleep(0.2)
