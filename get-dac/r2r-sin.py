a02ae9df3bb005e7b5c876532594a7a5e36b38f9
tse2veronika <tsetva.vi@phystech.edu>
2026-02-25 15:12:52 +0300

    еще раз добавлен 8-битный синус

---
+++ get-dac/r2r-sin.py
@@ -0,0 +1,6 @@
+import r2r_dac as r2r
+import signal_generator as sg
+import time
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
---
+++ get-dac/signal_generator.py
@@ -0,0 +1,6 @@
+import numpy
+import time
+def get_sin_wave_amplitude(freq, time):
+    return (numpy.sin(2 * numpy.pi * freq * time) + 1) / 2
+def wait_for_sampling_period(sampling_frequency):
+    time.sleep(1 / sampling_frequency)
